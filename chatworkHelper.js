176b0ff34bf1203f0e6ae72b22e9d0d46a77a8a4
egmc <waltz4debby@eg2mix.com>
2017-02-12 00:06:43 +0900

    fix all open button disabled due to  style changing

--- chatworkHelper.js
+++ chatworkHelper.js
@@ -158,7 +158,7 @@
   $('#_openedButton').on('click', function(e) {
     var d=new Date();
     var rid;
-    $('.roomUnread').each(function(i, element){
+    $("li.roomListItem:has(p.roomListItem__roomName--unread)").each(function(i, element){
         rid = $(element).data('rid');
         $.getJSON('https://'+window.location.hostname+'/gateway.php?cmd=read&myid='+myid+'&_v='+client_ver+'&_av=4&_t='+ACCESS_TOKEN+'&ln=ja&room_id='+rid+'&last_chhkkt_id='+$('._message[data-rid='+rid+']:last').data('mid')+'&_='+d.getDate());
     });
