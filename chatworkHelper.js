5540859e4864b2249f73ee84cb239b3813b72b1d
ueno.ryoichi <ueno.ryoichi@lancers.co.jp>
2017-02-13 09:38:51 +0900

    バージョンの変更

--- chatworkHelper.js
+++ chatworkHelper.js
@@ -2,7 +2,7 @@
 // @name         chatwork helper
 // @match        https://www.chatwork.com/*
 // @match        https://kcw.kddi.ne.jp/*
-// @version      1.5.1
+// @version      1.5.2
 /* load jQuery */
 // @require https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
 // ==/UserScript==
